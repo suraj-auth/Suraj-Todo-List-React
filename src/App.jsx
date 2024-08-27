3a664b64d5387abd298fd254e8e2a3e0517e3677
suraj-auth <surajmehto93@gmail.com>
2024-08-27 22:39:28 +0530

    changing rounded app corners

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 function App() {
   return (
-    <div className="border-2 border-black w-full h-full sm:w-2/6 sm:h-full m-auto bg-green-200 px-2 rounded-xl dark:bg-slate-800">
+    <div className="border-2 border-black w-full h-full sm:w-2/6 sm:h-full m-auto bg-green-200 px-2 dark:bg-slate-800">
       <Nav />
       <Home />
     </div>
