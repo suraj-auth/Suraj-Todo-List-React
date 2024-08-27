0c74e3773d917bdd1f83481a4a632fc162cd2f00
suraj-auth <surajmehto93@gmail.com>
2024-08-27 22:47:32 +0530

    adding padding in bottom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 function App() {
   return (
-    <div className="border-2 border-black w-full min-h-full sm:w-2/6 sm:min-h-full m-auto bg-green-200 px-2 dark:bg-slate-800">
+    <div className="border-2 border-black w-full pb-6 min-h-full sm:w-2/6 sm:min-h-full m-auto bg-green-200 px-2 dark:bg-slate-800">
       <Nav />
       <Home />
     </div>
